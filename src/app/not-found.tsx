4e89bfecb3b637f9f8d901ec87d5a44ac1842e07
italy <italy.christian@hotmail.com>
2025-09-19 10:11:51 -0300

    creating error boundary

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,11 @@
 import { ErrorMessage } from '@/components/ErrorMessage';
-import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
     <ErrorMessage
       pageTitle='Página não encontrada'
       contentTitle='404'
-      content='  Erro 404 - A página que você está tentando acessar não existe neste
+      content='Erro 404 - A página que você está tentando acessar não existe neste
             site.'
     />
   );
